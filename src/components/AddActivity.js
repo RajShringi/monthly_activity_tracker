98a4ddab5be1f7f16d70d8cb94c9ff5ea65ff485
raj shringi <raj24shringi@gmail.com>
2022-09-21 20:18:12 +0530

    add AddActivity component and handle state for input and allActivities

---
+++ src/components/AddActivity.js
@@ -0,0 +1,24 @@
+function AddActivity(props) {
+  return (
+    <form
+      onSubmit={props.handleSubmit}
+      className="max-w-md mx-auto text-center"
+    >
+      <input
+        className="border py-2 px-12 shadow-inner rounded-tl-lg rounded-bl-lg outline-none hover:border-gray-300"
+        type="text"
+        name="activity"
+        value={props.activity}
+        placeholder="e.g coding"
+        onChange={props.handleInput}
+      />
+      <button
+        className="text-slate-200 bg-teal-400 py-2 px-3 rounded-br-lg rounded-tr-lg font-light border border-teal-400 hover:bg-teal-500 hover:border-teal-500"
+        type="submit"
+      >
+        Add Activity
+      </button>
+    </form>
+  );
+}
+export default AddActivity;
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,44 @@
 import React from "react";
+import AddActivity from "./AddActivity";
 class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      activity: "",
+      allActivities: [],
+    };
+  }
+  handleInput = (e) => {
+    this.setState({
+      activity: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => {
+      return {
+        allActivities: [...prevState.allActivities, prevState.activity],
+        activity: "",
+      };
+    });
+  };
   render() {
-    return <div>Hello</div>;
+    return (
+      <div className="container mx-auto text-gray-700">
+        <header className="text-center py-8 mb-6">
+          <h1 className="text-4xl font-semibold text-cyan-500">
+            Monthly Activity Tracker!
+          </h1>
+        </header>
+        <main>
+          <AddActivity
+            activity={this.state.activity}
+            handleInput={this.handleInput}
+            handleSubmit={this.handleSubmit}
+          />
+        </main>
+      </div>
+    );
   }
 }
 export default App;
